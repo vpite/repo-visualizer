188c55bace88838d9f4a8cf55a1338ba00134ed6
vpite <122662544+vpite@users.noreply.github.com>
2023-02-12 23:21:49 +0000

    wip

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,11 @@ import { processDir } from "./process-dir.js";
 import { Tree } from "./Tree.tsx";
 
 const main = async () => {
-  const outputFile = "./diagram.svg";
-  const rootPath = ""; // Micro and minimatch do not support paths starting with ./
+  const args = process.argv.slice(2);
+  const [rootPath = ""] = args; // Micro and minimatch do not support paths starting with ./
   const maxDepth = 9;
   const customFileColors = JSON.parse("{}");
   const colorEncoding = "type";
-  const commitMessage = "Repo visualizer: update diagram";
   const excludedPathsString =
     "node_modules,bower_components,dist,out,build,eject,.next,.netlify,.yarn,.git,.vscode,package-lock.json,yarn.lock";
   const excludedPaths = excludedPathsString.split(",").map((str) => str.trim());
@@ -19,6 +18,8 @@ const main = async () => {
   // Split on semicolons instead of commas since ',' are allowed in globs, but ';' are not + are not permitted in file/folder names.
   const excludedGlobsString = "";
   const excludedGlobs = excludedGlobsString.split(";");
+
+  const outputFile = "./diagram.svg";
 
   const data = await processDir(rootPath, excludedPaths, excludedGlobs);
 
